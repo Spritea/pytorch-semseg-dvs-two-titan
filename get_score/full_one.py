correct pre/rec formula

get_score/full_one.py
@@ -60,7 +60,7 @@ GT_Str = []
 for j in GT_File:
     GT_Str.append(str(j))
 
-th=0.98
+th=0
 # th=list(np.arange(0,0.99,0.02))
 pre=[]
 rec=[]
@@ -102,8 +102,8 @@ acc, cls_pre, cls_rec, cls_f1, cls_iu, hist = running_metrics_val.get_scores()
 tt = time.time() - t
 pre.append(cls_pre)
 rec.append(cls_rec)
-np.save("pre",pre)
-np.save("rec",rec)
+# np.save("pre",pre)
+# np.save("rec",rec)
 print("cls pre")
 print(cls_pre)
 print("cls rec")

get_score/metrics_my.py
@@ -29,7 +29,7 @@ class runningScore(object):
         t2=lp.flatten()
         fh=self._fast_hist(lt.flatten(), lp.flatten(), self.n_classes)
         self.confusion_matrix += fh
-        # print("kk")
+        ceshi=fh
 
     def get_scores(self):
         """Returns accuracy score evaluation result.
@@ -48,8 +48,12 @@ class runningScore(object):
         freq = hist.sum(axis=1) / hist.sum()
         fwavacc = (freq[freq > 0] * iu[freq > 0]).sum()
         cls_iu = dict(zip(range(self.n_classes), iu))
-        cls_pre = np.diag(hist) / hist.sum(axis=1)
-        cls_rec = np.diag(hist) / hist.sum(axis=0)
+        # cls_pre = np.diag(hist) / hist.sum(axis=1)
+        # cls_rec = np.diag(hist) / hist.sum(axis=0)
+        #axis=0 才是竖着求和,axis=1是横着求和
+        #此处混淆矩阵 右上角表头是预测值，左下角表头是真实值
+        cls_pre = np.diag(hist) / hist.sum(axis=0)
+        cls_rec = np.diag(hist) / hist.sum(axis=1)
 
         cls_f1 = 2 * cls_pre * cls_rec / (cls_pre + cls_rec)
         mean_f1 = np.nanmean(cls_f1)